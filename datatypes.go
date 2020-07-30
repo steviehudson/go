64e678ca0ea961af9195ff18373602e6e7c02983
steviehudson <steviehudson@outlook.com>
2020-07-30 20:48:50 +0100

    datatypes

---
+++ datatypes.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+)
+
+func datatypes() {
+
+	firstName, lastName := "Stevie", "Hudson"
+
+	fmt.Println(firstName, lastName)
+
+	//built in maths
+	com := complex(1, 2)
+	r, i := real(com), imag(com)
+
+	fmt.Println(com)
+	fmt.Println(r, i)
+
+	//declare pointer
+	var location *string = new(string)
+
+	//dereference
+	*location = "location"
+
+	fmt.Println(location)
+	fmt.Println(*location)
+
+	//address of
+	address := "address"
+	pointer := &address
+
+	fmt.Println(address)
+	fmt.Println(*pointer, pointer)
+
+	address = "new address"
+	fmt.Println(*pointer, pointer)
+
+	//constant cannot change value, must assign on same line before compile
+	const pi = 3.14
+	fmt.Println(pi)
+
+	//infer datatype, evaluates each time
+	const infer = 1
+	fmt.Println(infer + 1)
+	fmt.Println(infer + 1.2)
+
+	//convert explict datatype
+	const inferEx int = 1
+	fmt.Println(float32(inferEx) + 1.2)
+}
---
+++ iotatest.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+)
+
+const (
+	global  = "global"
+	another = "another"
+
+	// iota const
+	iotaGlobal  = iota
+	iotaAnother = iota
+
+	// math
+	iotaCurrent = iota
+	iotaPlus    = iota + 1
+	iotaUnspecified1
+	iotaBitshift = 2 << iota
+	iotaUnspecified2
+)
+
+const (
+	// reset iota with new const block
+	iotaReset = iota
+)
+
+func iotatest() {
+
+	fmt.Println(global, another)
+	fmt.Println(iotaGlobal, iotaAnother)
+	fmt.Println(iotaCurrent, iotaPlus, iotaUnspecified1, iotaBitshift, iotaUnspecified2)
+	fmt.Println(iotaReset)
+	fmt.Println(iotaGlobal, iotaReset)
+}
